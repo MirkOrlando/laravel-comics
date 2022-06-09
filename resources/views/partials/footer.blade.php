abca1151d998e4bd8c8c5e1574c73550f88b5b08
MirkOrlando <mirko@orlando-m.it>
2022-06-09 17:00:38 +0200

    create footer-bottom

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -63,5 +63,25 @@
             </div>
         </div>
     </div>
-
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row jc-space-between ai-center pb-2 pt-2">
+                <div class="col">
+                    <a href="#" class="btn-primary-outline">Sign-up now!</a>
+                </div>
+                <div class="col">
+                    <div class="social">
+                        <div class="text">follow us</div>
+                        <div class="social-icons">
+                            <img src="{{ asset('img/footer-facebook.png') }}" alt="" />
+                            <img src="{{ asset('img/footer-twitter.png') }}" alt="" />
+                            <img src="{{ asset('img/footer-youtube.png') }}" alt="" />
+                            <img src="{{ asset('img/footer-pinterest.png') }}" alt="" />
+                            <img src="{{ asset('img/footer-periscope.png') }}" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
